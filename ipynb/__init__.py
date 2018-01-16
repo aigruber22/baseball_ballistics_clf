b85f0937f1e06bea5bb7d68070f8584fff456bad
Adam Gruber <adam.i.gruber@gmail.com>
2018-01-15 18:01:31 -0800

    Final project clean-up.

--- ipynb/__init__.py
+++ ipynb/__init__.py
@@ -28,8 +28,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, classification_report, accuracy_score, log_loss, f1_score, fbeta_score, cohen_kappa_score
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import resample
